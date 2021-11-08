1456a0353b5c18076143d9304108c81700c74cbb
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-11-08 15:53:33 -0500

    thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -61,7 +61,7 @@ const thoughtController = {
           res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
-        res.json(dbPizzaData);
+        res.json(dbUserData);
       })
       .catch((err) => res.json(err));
   },
@@ -69,11 +69,52 @@ const thoughtController = {
   // delete user
   deleteThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.id })
-      .then((dbUserData) => res.json(dbUserData))
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: "No thought found by this id" });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // add a reaction, api/thoughts/:thoughtsId/reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reaction: body } },
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: "Reaction not found!" });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
       .catch((err) => res.json(err));
   },
-  // reaction
   //delete reation
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: "No thoughts with this id!" });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = thoughtController;
